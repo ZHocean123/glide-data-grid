0b6ef191728c957b127de08f591aba2a779dd08a
ocean <youshanghaiyang@gmail.com>
2025-09-30 16:15:15 +0800

    refactor(data-grid-overlay-editor): 移除气泡编辑器样式组件并内联样式
    将气泡编辑器的样式从单独组件移入主组件，简化组件结构

--- packages/vue-core/src/internal/data-grid-overlay-editor/private/bubbles-overlay-editor.vue
+++ packages/vue-core/src/internal/data-grid-overlay-editor/private/bubbles-overlay-editor.vue
@@ -1,5 +1,5 @@
 <template>
-  <BubblesOverlayEditorStyle>
+  <div class="BubblesOverlayEditorStyle">
     <div
       v-for="(b, i) in bubbles"
       :key="i"
@@ -8,15 +8,45 @@
       {{ b }}
     </div>
     <textarea class="gdg-input" autofocus />
-  </BubblesOverlayEditorStyle>
+  </div>
 </template>
 
 <script setup lang="ts">
-import BubblesOverlayEditorStyle from './bubbles-overlay-editor-style.vue';
-
-interface Props {
-  readonly bubbles: readonly string[];
-}
-
-const props = defineProps<Props>();
+defineProps<{
+  bubbles: readonly string[]
+}>();
 </script>
+<style lang="scss">
+.BubblesOverlayEditorStyle {
+  display: flex;
+    flex-wrap: wrap;
+    margin-top: auto;
+    margin-bottom: auto;
+    overflow: auto;
+
+    .boe-bubble {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        border-radius: var(--gdg-rounding-radius, calc(var(--gdg-bubble-height) / 2));
+
+        padding: 0 var(--gdg-bubble-padding);
+        height: var(--gdg-bubble-height);
+        background-color: var(--gdg-bg-bubble);
+        color: var(--gdg-text-dark);
+        margin: var(--gdg-bubble-margin);
+        white-space: nowrap;
+    }
+
+    textarea {
+        position: absolute;
+        top: 0px;
+        left: 0px;
+        width: 0px;
+        height: 0px;
+
+        opacity: 0;
+    }
+}
+</style>
